e89b4cfb7fb72802dfe08787d1caff70926e6512
Sargun Vohra <sargun.vohra@gmail.com>
2019-05-29 23:42:35 -0700

    install prebuilt packages

--- modsman.rb
+++ modsman.rb
@@ -1,21 +1,30 @@
 class Modsman < Formula
   desc "Minecraft mod manager for the command-line"
   homepage "https://gitlab.com/sargunv-mc-mods/modsman"
-  url "https://gitlab.com/sargunv-mc-mods/modsman.git",
-      :tag      => "0.20.1",
-      :revision => "b23331f7080b5a2718ab43e4120372bcf3421b6d"
+  head "https://gitlab.com/sargunv-mc-mods/modsman.git"
+
+  # for new versions, update the url's build number and artifact name, the sha256, and the version
+  url "https://dev.azure.com/sargunvohra/modsman/_apis/build/builds/37/artifacts?artifactName=modsman-cli-0.20.1&api-version=5.0&%24format=zip"
+  sha256 "136609552bbddbe3dbbd64c6a7731e8ff7cb8fcf8b92532172142f3b0d3d9da2"
+  version "0.20.1"
 
   depends_on :java => "11+"
 
   def install
-    system "./gradlew", ":modsman-cli:installDist"
-    libexec.install Dir["modsman-cli/build/install/modsman-cli/*"]
+    if build.head?
+      system "./gradlew", ":modsman-cli:installDist"
+      libexec.install Dir["modsman-cli/build/install/modsman-cli/*"]
+    else
+      libexec.install %w[bin lib]
+      system "chmod", "+x", "#{libexec}/bin/modsman-cli"
+    end
+
     rm_f Dir["#{libexec}/bin/*.bat"]
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
-    # TODO: replace this with a proper test
-    system "#{bin}/modsman-cli", "--version"
+    (testpath/".modlist.json").write('{"config": {"game_version": "1.14"}, "mods": []}')
+    system bin/"modsman-cli", "list"
   end
 end
