2ba331009c492a2af862ecd1b5393b855e4ef1bb
Sargun Vohra <sargun.vohra@gmail.com>
2020-04-16 01:05:18 -0700

    Update modsman.rb

--- modsman.rb
+++ modsman.rb
@@ -3,9 +3,9 @@ class Modsman < Formula
   homepage "https://gitlab.com/sargunv-mc-mods/modsman"
   head "https://gitlab.com/sargunv-mc-mods/modsman.git"
 
-  url "https://github.com/sargunv/modsman/releases/download/0.22.0/modsman-cli-0.22.0.zip"
-  sha256 "66a63a05e7b2068a8e0480ebbb29a552f751c1c504f1982ae392ea3e255dba83"  # MUST BE LOWERCASE
-  version "0.22.0"
+  url "https://github.com/sargunv/modsman/releases/download/0.22.0/modsman-cli-0.30.0.zip"
+  sha256 "83d43a470be0f37639cc6da9bb59a4a1fc6331c19fb6c4a58048e0582fc32b89"  # MUST BE LOWERCASE
+  version "0.30.0"
 
   depends_on :java => "11+"
 
